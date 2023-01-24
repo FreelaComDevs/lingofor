b5c733327e85ccb5369095547fc7ea387846be19
Fernando Bonin <fernando.bonin@hotmail.com.br>
2023-01-24 11:08:35 -0300

    area de dados com link para my account

--- lingoforme/src/components/_common/header/Header.js
+++ lingoforme/src/components/_common/header/Header.js
@@ -1,29 +1,31 @@
 import React from 'react'
 
 import { Head } from './styles';
+import { AvatarArea } from '../avatar/avatar';
 
-const Header = ({title, icon, children}) => {
-  
+const Header = ({ title, icon, children }) => {
+
   return (
 
     <Head>
       <header>
-          <div className="header-holder">
-            <div>
-              <img src={"https://img.freepik.com/vetores-gratis/astronauta-bonito-dabbing-ilustracao-do-icone-dos-desenhos-animados-icone-de-ciencia-espacial-isolado-estilo-flat-cartoon_138676-3101.jpg?w=2000"}/>
-            </div>
-            <div className='personalData'>
-              <h3>Vini</h3>
-              <p>Lingua Nativa: português</p>
-              <p>País: Brasil</p>
-              <p>Desde: abril/2023</p>
-            </div>
+        <div className="header-holder">
+          <div className='container' style={{alignItems:"end"}}>
+            <AvatarArea             
+            name={"Vini"}
+            language={"Português"}
+            country={"Brasil"}
+            date={"abril/2020"}
+            href={"/manage-account"}
+            src={"https://img.freepik.com/vetores-gratis/astronauta-bonito-dabbing-ilustracao-do-icone-dos-desenhos-animados-icone-de-ciencia-espacial-isolado-estilo-flat-cartoon_138676-3101.jpg?w=2000"}
+            /> 
           </div>
-            { title === '' &&
-              {children}
-            }
+        </div>
+        {title === '' &&
+          { children }
+        }
 
-            {/* { title !== '' &&
+        {/* { title !== '' &&
             <h1>
               {icon}
               <span>{title}</span>
--- lingoforme/src/components/_common/header/styles.js
+++ lingoforme/src/components/_common/header/styles.js
@@ -3,7 +3,7 @@ import bg from '../../../images/bg-header.svg';
 
 export const Head = styled.div`
     width: 100%;    
-
+    
     h1{
         padding: 32px 0 80px 85px;
         color: #004FFF;
@@ -18,28 +18,18 @@ export const Head = styled.div`
         margin: 0 10px 0 0;
         height: 40px;
         border-radius:100%;
-    }
-
-    .personalData{
-        font-size:14px;
-    }
-
-    .personalData > h3 {
-        font-weight: 700;
-    }
+    }    
 
     .header-holder {
         background-image: url(${bg});
         background-position: 100% -4px;
         background-repeat: no-repeat;
         height: 120px;
-        width: 100%;
-        float: right;
+        width: 100%;        
         margin-bottom: 72px;
-        display:flex;
-        flex-direction:column;
-    } 
-
+        display:flex;    
+    }
+    
     @media (min-width: 320px) and (max-width: 1024px) {
 
     header{

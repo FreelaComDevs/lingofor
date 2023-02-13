2dbce7279d10a818463e1df87014a72d2f842ac5
Fernando Bonin <fernando.bonin@hotmail.com.br>
2023-02-13 12:45:03 -0300

    headers padronizados

--- lingoforme/src/components/Notifications/Notifications.js
+++ lingoforme/src/components/Notifications/Notifications.js
@@ -4,7 +4,7 @@ import { translate } from "react-i18next";
 import { withRouter, Link } from 'react-router-dom'
 import { getUserNotifications, setNotificationMessage, unsetNotificationMessage } from "../../actions/notificationActions";
 import SideMenu from "../_common/SideMenu/SideMenu";
-import Header from "../_common/header/NewHeader";
+import Header from "../_common/header/Header";
 import Loading from "react-fullscreen-loading";
 import classIcon from "../../images/icons/icon_notifications_header.svg";
 import noNotifications from "../../images/placeholder/notifications_empty.png";
@@ -29,28 +29,28 @@ function Transition(props) {
 }
 
 class Notifications extends Component {
-  state = this.initialState    
-	
-	get initialState() {
-		return {
-			filters: {
-				// id: "",
+  state = this.initialState
+
+  get initialState() {
+    return {
+      filters: {
+        // id: "",
         // creatorUserId: "",
         // role: "",
         // ticketTypeId: "",
         // ticketSubTypeId: "",
         // createdAt: "",
-				// status: "",
-				pageNumber: 1,
-				pageSize: 50,
+        // status: "",
+        pageNumber: 1,
+        pageSize: 50,
       },
       userId: null
-		}
-	}
-  componentWillMount(){
-		this.getPageNumberCurrent()
+    }
   }
-  
+  componentWillMount() {
+    this.getPageNumberCurrent()
+  }
+
   componentDidMount() {
     const getUserInfo = () => {
       const token = JSON.parse(localStorage.getItem("@lingo")).token;
@@ -68,21 +68,21 @@ class Notifications extends Component {
 
   getPageNumberCurrent = () => {
 
-		const params = queryString.parse(this.props.location.search);
-		const { filters }  = this.state
+    const params = queryString.parse(this.props.location.search);
+    const { filters } = this.state
 
-		if(params && params["page"]){
-			filters["pageNumber"] = Number(params["page"])
-			this.setState({filters});
-		}
-	}
+    if (params && params["page"]) {
+      filters["pageNumber"] = Number(params["page"])
+      this.setState({ filters });
+    }
+  }
 
-	pagination = (page, type) => {
-		const { filters }  = this.state
-		filters["pageNumber"] = Number(page)
-		this.setState({filters})
-		this.applyFilters()
-	}
+  pagination = (page, type) => {
+    const { filters } = this.state
+    filters["pageNumber"] = Number(page)
+    this.setState({ filters })
+    this.applyFilters()
+  }
 
   getUserInfo = () => {
     const token = JSON.parse(localStorage.getItem("@lingo")).token;
@@ -96,19 +96,19 @@ class Notifications extends Component {
     let { filters, userId } = this.state
     filters.id = userId
     this.props.getUserNotifications(filters);
-  } 
+  }
 
   resetFilter = async () => {
     this.props.getUserNotifications(this.initialState.filters);
-    this.setState({ filters: this.initialState.filters })   
+    this.setState({ filters: this.initialState.filters })
   }
-  
-  
+
+
 
   setMessage = (id, message) => {
     const user = this.getUserInfo();
     const userId = user.id
-    this.props.setNotificationMessage({id, message, userId})
+    this.props.setNotificationMessage({ id, message, userId })
   };
 
   unsetMessage = () => {
@@ -123,83 +123,86 @@ class Notifications extends Component {
   render() {
     const user = this.getUserInfo();
     const { props, setMessage, unsetMessage, state } = this;
-    const { filters: { pageNumber }} = state;
-    const { t, notifications: { userNotifications, loading, message, totalPages, totalFound }} = props;
+    const { filters: { pageNumber } } = state;
+    const { t, notifications: { userNotifications, loading, message, totalPages, totalFound } } = props;
     const itemKey = t("LANGUAGE_KEY_STRING");
 
-      const notifications = userNotifications.items
+    const notifications = userNotifications.items
       ? userNotifications.items.map(
-            ({
-                id,
-                createdAt,
-                read,
-                notificationSent: { module, subject, message }
-            }) => ({ id, createdAt, read, module, subject, message })
-          )
+        ({
+          id,
+          createdAt,
+          read,
+          notificationSent: { module, subject, message }
+        }) => ({ id, createdAt, read, module, subject, message })
+      )
       : [];
 
-      console.log('totalPages', totalPages)
+    console.log('totalPages', totalPages)
     return (
       <div className="view new-view notifications-view">
-        <SideMenu />
-        <Header title={t("NOTIFICATIONS")} icon={classIcon} />
-        <Loading
-          loading={loading}
-          background="rgba(0,0,0,0.6)"
-          loaderColor="#3498db"
-        />
-        <div className="new-container">
-          {user.role === "student" && <Link to="/manage-account/notification"><p className="settingsLink">{t("NOTIFICATION_SETTINGS")}</p></Link>}
-          { notifications.length > 0 
-            ? notifications.map(({ id, read, module, subject, createdAt, message }) => {
-              const messageSubject = subject
-              return (
-              <div className="new-box new-box-table-list new-box-notification">
-                <div className={`notificationInfo${!read ? " unReadNotification": ""}`}>
-                  <h4>{t('MODULE')}</h4>
-                  <p>{messageSubject}</p>
-                  <p className="fromNow">{cap(moment(createdAt).locale(t("LOCALE")).fromNow())}</p>
-                </div>
-                <button
-                  className="view-button"
-                  onClick={() => setMessage(id, {messageSubject, message})}
-                >
-                  {t("VIEW")}{" "}
-                  <i className="fa fa-angle-right" aria-hidden="true" />
-                </button>
-              </div>
-              
-            )})
-            : <Fragment>
+        <section>
+          <SideMenu />
+          <Header title={t("NOTIFICATIONS")} icon={classIcon} />
+          <Loading
+            loading={loading}
+            background="rgba(0,0,0,0.6)"
+            loaderColor="#3498db"
+          />
+          <div className="new-container" style={{marginLeft: '0'}}>
+            {user.role === "student" && <Link to="/manage-account/notification"><p className="settingsLink">{t("NOTIFICATION_SETTINGS")}</p></Link>}
+            {notifications.length > 0
+              ? notifications.map(({ id, read, module, subject, createdAt, message }) => {
+                const messageSubject = subject
+                return (
+                  <div className="new-box new-box-table-list new-box-notification">
+                    <div className={`notificationInfo${!read ? " unReadNotification" : ""}`}>
+                      <h4>{t('MODULE')}</h4>
+                      <p>{messageSubject}</p>
+                      <p className="fromNow">{cap(moment(createdAt).locale(t("LOCALE")).fromNow())}</p>
+                    </div>
+                    <button
+                      className="view-button"
+                      onClick={() => setMessage(id, { messageSubject, message })}
+                    >
+                      {t("VIEW")}{" "}
+                      <i className="fa fa-angle-right" aria-hidden="true" />
+                    </button>
+                  </div>
+
+                )
+              })
+              : <Fragment>
                 <p className="placeholderText">{t("NO_NOTIFICATIONS")}</p>
-                <img className="placeholderImage" src={noNotifications}/>
+                <img className="placeholderImage" src={noNotifications} />
               </Fragment>
-              
-        }
-        </div>
-        <Pagination pageAtual={pageNumber} pageSize={50}  totalPages={totalPages} totalFound={totalFound} onClick={(page, type) => this.pagination(page, type)}/>
-        <Dialog
-          id="dialog-notification"
-          open={!!message}
-          TransitionComponent={Transition}
-          keepMounted
-          onClose={this.props.unsetMessage}
-        >
-          <DialogTitle id="dialog-notification-title">
-            {message.messageSubject}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="dialog-notification-message">
-              {/* {message.message} */}
-              {nl2br(message.message) }
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={unsetMessage} >
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
+
+            }
+          </div>
+          <Pagination pageAtual={pageNumber} pageSize={50} totalPages={totalPages} totalFound={totalFound} onClick={(page, type) => this.pagination(page, type)} />
+          <Dialog
+            id="dialog-notification"
+            open={!!message}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={this.props.unsetMessage}
+          >
+            <DialogTitle id="dialog-notification-title">
+              {message.messageSubject}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="dialog-notification-message">
+                {/* {message.message} */}
+                {nl2br(message.message)}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={unsetMessage} >
+                OK
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </section>
       </div>
     );
   }

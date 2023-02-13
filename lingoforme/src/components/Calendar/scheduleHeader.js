897a34b88d20d05ec7ec23656d280a1b9b81a871
Fernando Bonin <fernando.bonin@hotmail.com.br>
2023-02-13 12:12:10 -0300

    remocao de botoes do headerSchedule

--- lingoforme/src/components/Calendar/scheduleHeader.js
+++ lingoforme/src/components/Calendar/scheduleHeader.js
@@ -3,17 +3,13 @@
 export const HeaderSchedule = ({ date }) => {
     return (
         <div className='flex flex-col gap-5 p-4 ml-10'>
-            <div className='flex justify-between gap-3'>
-                <span className='text-base font-semibold text-primary'>{date.format('MMM D')}</span>
-                <button className='px-4 py-1 text-base text-white rounded-full bg-sucess'>Ativar agendamento automatico</button>
+            <div>
+                <span className='text-base font-semibold text-primary'>{date.format('MMM D')}</span>                
             </div>
-            <div className='flex justify-between gap-3'>
+            <div>
                 <button className='px-4 py-1 text-base duration-300 bg-white border rounded-md border-btn-class-confirmed text-btn-class-confirmed hover:bg-btn-class-confirmed hover:text-white hover:border-transparent'>
                     Você tem 2 aulas agendadas
-                </button>
-                <button className='px-4 py-1 text-base duration-300 bg-white border rounded-md border-warning text-warning hover:bg-warning hover:text-white hover:border-transparent'>
-                    1 formulário para preencher
-                </button>
+                </button>                
             </div>
         </div>
     )
